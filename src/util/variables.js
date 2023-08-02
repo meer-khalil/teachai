029194c523c9b7a6cbf674b2c072b63beedfcabe
khalilahmad3212 <arzishjani1613@gmail.com>
2023-08-02 10:51:38 -0700

    updated backendurl from http to https

--- src/util/variables.js
+++ src/util/variables.js
@@ -1,5 +1,5 @@
 // export const site = null
 // export const backend_url = 'http://localhost:4000/api/v1'
 
-export const site = 'http://www.teachassistai.com'
-export const backend_url = 'http://www.teachassistai.com/api/v1'
+export const site = 'https://www.teachassistai.com'
+export const backend_url = 'https://www.teachassistai.com/api/v1'

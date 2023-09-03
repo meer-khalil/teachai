035fddd93de3471331b0c26dbe659625b371f6c4
khalilahmad3212 <arzishjani1613@gmail.com>
2023-09-03 12:05:39 -0700

    number format fixed

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -23,7 +23,6 @@ const Form = () => {
                     <label for="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone Number</label>
                     <input 
                         type="phone"
-                        pattern='(d{3})-d{4}-d{3}' 
                         id="phone"
                         className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="(201)-1234-1234" required />
                 </div>
